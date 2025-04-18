bd7942ba8c6062ddb5db837c62e757ada521288d
tachen <tatoschka1234@users.noreply.github.com>
2025-04-18 10:53:51 -0700

    pep

--- src/main/java/ru/yandex/practicum/filmorate/storage/FriendshipStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FriendshipStorage.java
@@ -6,10 +6,15 @@ import java.util.List;
 
 public interface FriendshipStorage {
     void add(Long userId, Long friendId, String status);
+
     boolean exists(Long userId, Long friendId);
+
     boolean existsWithStatus(Long userId, Long friendId, String status);
+
     void updateStatus(Long userId, Long friendId, String status);
+
     void remove(Long userId, Long friendId);
+
     List<User> getCommonFriends(Long userId, Long otherUserId);
 }
 
